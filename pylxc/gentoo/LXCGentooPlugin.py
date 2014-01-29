ced67b9a09fb0b33213ddcbb3da4ff25f32a5c52
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2014-01-29 22:58:15 +0100

    #2: Added stub for gentoo plugin

--- pylxc/gentoo/LXCGentooPlugin.py
+++ pylxc/gentoo/LXCGentooPlugin.py
@@ -1,20 +1,27 @@
-from  LXCPlugin import LXCPlugin
+import LXCPlugin
 import logging
 
 log = logging.getLogger("pylxc.gentoo")
 
+class LXCGentooCommand(LXCPlugin.LXCPluginCommand):
+    def __init__(self):
+        pass
+    def __call__(self, parsedArgs, lxcConfiguration):
+        log.info("LXC gentoo guest plugin")
+        log.debug("Args: {0}".format(parsedArgs))
+
 def create(*args, **kwargs):
     log.info("Creating lxc gentoo guest")
 
-class LXCGentooPlugin(LXCPlugin):
+class LXCGentooPlugin(LXCPlugin.LXCPlugin):
     def __init__(self, *args, **kwargs):
-        LXCPlugin.__init__(self, *args, **kwargs)
+        LXCPlugin.LXCPlugin.__init__(self, *args, **kwargs)
 
     def register(self, argParser):
-        LXCPlugin.register(self, argParser)
+        LXCPlugin.LXCPlugin.register(self, argParser)
         subParser = argParser.addSubParser('gentoo', help="LXC gentoo managment")
         subParser.add_argument('create', help='Create lxc gentoo guest container')
-        subParser.setMainModule(create)
+        subParser.setMainModule(LXCGentooCommand())
 
 
 def createPlugin(*args, **kwargs):
