56d972696bc1f70e70ffc9578ec66d4a7b68efd4
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2014-01-29 22:45:25 +0100

    #6: Added parser for plugins command line

--- pylxc/pylxc.py
+++ pylxc/pylxc.py
@@ -2,40 +2,54 @@ import sys
 import argparse
 from logger import logger
 import logging
+import types
+import importlib
 
-class LXCPluginsScanner(object):
+
+log = logging.getLogger("pylxc.main")
+
+class LXCPluginsLoader(object):
     SUPPORTED_LXC_CONTAINERS = ['gentoo']
     def __init__(self):
         pass
 
-    def importPlugins(self):
-        pass
-
-class SubParser(argparse.ArgumentParser):
-    def __init__(self, **kwargs):
-        super(argparse.ArgumentParser, self).__init__(kwargs)
-
-    def setMainModule(self, mainModule):
-        self._subParserModule.set_defaults(commandMainModule=mainModule)
+    def importPlugins(self, argParser):
+        for moduleName in LXCPluginsLoader.SUPPORTED_LXC_CONTAINERS:
+            try:
+                module = importlib.import_module(moduleName)
+                plugin = module.createPlugin()
+                plugin.register(argParser)
+            except ImportError as error:
+                log.error("Cannot import {0}: {1} ".format(moduleName, error))
 
 
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
-        argparse.ArgumentParser.__init__(self,*args, **kwargs)
+        argparse.ArgumentParser.__init__(self, *args, **kwargs)
         self._subParsers = self.add_subparsers(dest="moduleName",
-                                               help="Distribution name to be prepared for lxc deployment")
+                                               title="Supported lxc guests",
+                                               description="Supported lxc guests",
+                                               help="Distribution name to be prepared for lxc deployment",
+                                               parser_class=argparse.ArgumentParser)
 
-    def addSubparser(self, *args, **kwargs):
-        subParser = self.add_parser(*args, **kwargs)
-        return SubParser(subParser)
+    def addSubParser(self, *args, **kwargs):
+        subParser = self._subParsers.add_parser(*args, **kwargs)
+        #extend subParser with setMainModule
+        def setMainModule(self, mainModuleName):
+            self.set_defaults(executeCommand=mainModuleName)
+        subParser.setMainModule = types.MethodType(setMainModule, subParser)
+        return subParser
 
 
 def main(argv = None):
     if argv is None:
-        argv = sys.argv
+        argv = sys.argv[1:]
 
     argParser = ArgumentParser()
     argParser.add_argument( '--verbose', '-v', action='count', help="Log more detailed output")
+    loader = LXCPluginsLoader()
+    loader.importPlugins(argParser)
+
     options = argParser.parse_args(argv)
     if options.verbose<1:
         logger.getLogger().setLevel(logging.ERROR)
@@ -44,6 +58,7 @@ def main(argv = None):
     else:
         logger.getLogger().setLevel(logging.INFO)
 
+    options.executeCommand(options, None)
 
 
 if __name__ == "__main__":
